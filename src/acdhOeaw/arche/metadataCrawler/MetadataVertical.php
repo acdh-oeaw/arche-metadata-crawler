ac4ffa64e76e7daf1e0c2b0ae73ce024a60192ba
Mateusz Żółtak <zozlak@zozlak.org>
2024-06-06 08:14:51 +0200

    License headers fixed

--- src/acdhOeaw/arche/metadataCrawler/MetadataVertical.php
+++ src/acdhOeaw/arche/metadataCrawler/MetadataVertical.php
@@ -3,7 +3,7 @@
 /*
  * The MIT License
  *
- * Copyright 2023 zozlak.
+ * Copyright 2023 Austrian Centre for Digital Humanities.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
